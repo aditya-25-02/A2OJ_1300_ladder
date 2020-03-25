29f6d67ef66e69bce79711dadbfcd6aeb96e12ca
Aditya <adityaverma3471@gmail.com>
2020-03-25 17:04:06 +0530

    25th march 2020

---
+++ EffectiveApproach.cpp
@@ -0,0 +1,64 @@
+// Aditya verma 
+#include <iostream>
+#define FASTIO ios_base::sync_with_stdio(false);std::cin.tie(NULL);std::cout.tie(NULL)
+#define rep(i,a,n) for( ll i=a ; i<n ; i++)
+#define per(i,a,n) for( ll i=n-1 ; i>=a ;i--)
+#define pb push_back
+#define TLE cerr<<"Time elapsed : "<<1000*(ld)clock()/(ld)CLOCKS_PER_SEC<<"ms\n"
+#define vi vector<int>
+#define ll long long int 
+#define pii pair <int,int> 
+#define db double
+#define deb(x) cerr<<#x<<" : "<<x<<"\n";
+#define all(x) x.begin(),x.end()
+ll min(ll a,ll b){ return a<b ? a:b;}
+ll max(ll a,ll b){ return b>a ? b:a;}
+ll gcd(ll a,ll b){ return b?gcd(b,a%b):a;}
+const ll mod=1000000007;
+const ll  N =1e5+3;
+
+ll pos[N];
+
+using namespace std;
+
+int main()
+{
+    FASTIO;
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	freopen("error.txt","w",stderr);
+	#endif
+
+	int f ; 
+	cin>>f;
+
+	int sum_of_fingers = 0;
+
+	rep(i,0,f)
+	  {
+	    int temp ;
+	    cin>>temp;
+	    sum_of_fingers+=temp;
+	  }
+
+	f++; // there are f+1 people in the count    
+
+	if(f%2==0)
+	  {
+	  	// sum must be even 
+	  	if(sum_of_fingers%2==0) cout<<"2\n"; //he can show even no of fingers 
+	  	else                    cout<<"3\n"; //he can show odd no of fingers
+	  }
+	else
+	  {
+	  	// sum must be odd
+	  	if(sum_of_fingers%2==0) cout<<"3\n";
+	  	else                    cout<<"2\n";	
+	  }  	
+
+    
+
+    return 0 ;
+       
+}
